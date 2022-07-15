3d106ba21a97fd42a4b3ff5c56c350cb4e69a70c
anibalsanic <anibalsaj252005@gmail.com>
2022-07-14 22:42:32 -0400

    added sample component fixes #13

---
+++ components/AnnouncementBar.tsx
@@ -0,0 +1,9 @@
+import styles from "../styles/AnnouncementBar.module.css";
+
+export default function AnnouncementBar() {
+    return (
+        <div className={styles.outer_box}>
+            <p>EPC Website  •  This is an announcement</p>
+        </div>
+    );
+}
